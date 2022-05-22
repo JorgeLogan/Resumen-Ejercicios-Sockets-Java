9a3e926abe0317ece1b7d255189580fbacbd710a
JorgeLogan <jorgeacenal@gmail.com>
2022-05-22 11:45:43 +0200

    Ejercicios completados

--- src/multicast/EmisorMulticast.java
+++ src/multicast/EmisorMulticast.java
@@ -1,6 +1,80 @@
 package multicast;
 
-// Emisor Multicast. Envia mensajes para que los reciban los usuarios receptores Multicast
-public class EmisorMulticast {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 
+import interfaz.InterfazConexiones;
+
+// Emisor Multicast. Envia mensajes para que los reciban los usuarios receptores Multicast.
+// Muy similar al de UDP, solo que la InetAddress apunta al grupo, y no al IP localhost
+public class EmisorMulticast implements InterfazConexiones{
+	// Atributos
+	private MulticastSocket mcSocket; // Inicializado sin puerto. Se le da en el paquete
+	private InetAddress inetAddress; // En este caso, es el inetAddress del grupo, una IP 225.x.x.x. p.e.
+	private InetSocketAddress inetSocketAddress;
+	private NetworkInterface networkInterface; // Esta si llevara la IP del localhost
+	private DatagramPacket paquete;
+	private BufferedReader lector;
+	private boolean salir = false;
+	
+	
+	// Constructor
+	public EmisorMulticast() {
+		try {
+			this.inicializar();
+			
+			while(this.salir == false) {
+				this.ejecucionEnBucle();
+			}
+			
+			this.finalizar();
+		}
+		catch(Exception e) {
+			
+		}
+	}
+	
+	
+	// Metodo principal
+	public static void main(String[] args) {
+		new EmisorMulticast();
+	}
+	
+	@Override
+	public void inicializar() throws Exception {
+		this.mcSocket = new MulticastSocket(); // SIN el puerto.
+		this.inetAddress = InetAddress.getByName(GRUPO_MUTICAST); // Apuntando al grupo
+		this.lector = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Emisor Multicast inicializado");
+	}
+
+	@Override
+	public void ejecucionEnBucle() throws Exception {
+		while(salir == false) {
+			// Leemos los datos a enviar por consola
+			System.out.println("Escribe un mensaje para enviar:");
+			String mensaje = this.lector.readLine();
+			
+			// Los transformamos a byte[]
+			byte[] mensajeBytes = mensaje.getBytes();
+			
+			// Los pasamos al datagrma y lo enviamos
+			this.paquete = new DatagramPacket(mensajeBytes, mensajeBytes.length, this.inetAddress, PUERTO);
+			this.mcSocket.send(paquete);
+			
+			// Comprobamos si es un mensaje de finalizacion o no, para cerrar el bucle
+			if(mensaje.trim().equals(MENSAJE_FIN)) this.salir = true;
+		}
+	}
+
+	@Override
+	public void finalizar() throws Exception {
+		this.mcSocket.close();
+		System.out.println("Emisor multicast finalizado");
+	}
 }
--- src/multicast/ReceptorMulticast.java
+++ src/multicast/ReceptorMulticast.java
@@ -1,42 +1,82 @@
 package multicast;
 
 import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 
 import interfaz.InterfazConexiones;
 
-// Clase receptor multicast. Es muy similar al de UDP. El diferente es el emisor
+// Clase receptor multicast. Tiene varios cambios respecto al de UDP
+// Al pertenecer a un grupo multicast, necesitaremos:
+//		* Un inetAddress que apunte a un grupo, no a una IP
+//		* Un inetSocketAddress que usará el inetAddress del grupo con el puerto de escucha
+//		* Un NetworkInterface que apuntará a la IP localhost
+//		* Cuando tengamos todo, nos uniremos al grupo para escuchar, y lo abandoremos al finalizar
 public class ReceptorMulticast implements InterfazConexiones{
 	// Atributos
 	private MulticastSocket mcSocket;
+	private InetAddress inetAddress; // Apuntara al grupo
+	private InetSocketAddress inetSocketAddress; // Coge el grupo y puerto
+	private NetworkInterface networkInterface; // Funciona como el InetAddress en UDP cogiendo el localhost
 	private DatagramPacket paquete;
-	
+	private boolean salir = false;
 	
 	// Constructor
 	public ReceptorMulticast() {
-		
+		try {
+			this.inicializar();
+			
+			while(this.salir == false) {
+				
+				this.ejecucionEnBucle();
+			}
+			
+			this.finalizar();
+		}
+		catch(Exception e) {}
+	}
+	
+	
+	// Metodo principal
+	public static void main(String[] args) {
+		new ReceptorMulticast();
 	}
 	
 	
 	@Override
 	public void inicializar() throws Exception {
-		// TODO Auto-generated method stub
-		
+		this.mcSocket = new MulticastSocket(PUERTO); // Escuchamos el puerto
+		this.inetAddress = InetAddress.getByName(GRUPO_MUTICAST); // Apuntamos el inetAddress al grupo
+		this.inetSocketAddress = new InetSocketAddress(this.inetAddress, PUERTO); // Cogemos el inetadress y puerto
+		this.networkInterface = NetworkInterface.getByName(MI_HOST); // Escuchamos nuestro host igual que un inetaddress normal
+		this.mcSocket.joinGroup(this.inetSocketAddress, this.networkInterface); // Nos unimos al grupo
+;		System.out.println("Receptor Multicast inicializado");
 	}
 
 	@Override
 	public void ejecucionEnBucle() throws Exception {
-		// TODO Auto-generated method stub
+		// Preparamos el paquete y nos ponemos en escucha para rellenarlo con el mcsocket
+		byte[] mensaje = new byte[TAM_PAQUETE];
+		this.paquete = new DatagramPacket(mensaje, TAM_PAQUETE);
+		this.mcSocket.receive(paquete);
 		
+		// Transformamos el mensaje en cadena y eliminamos lo que no nos interesa con trim
+		String mensajeStr = new String(mensaje).trim();
+		
+		// Mostramos el mensaje
+		System.out.println("Paquete recibido: " + mensajeStr);
+		
+		// Comprobamos si es un mensaje de final para cerrar o seguir en el bucle
+		if(mensajeStr.equals(MENSAJE_FIN)) this.salir = true;
 	}
 
 	@Override
 	public void finalizar() throws Exception {
-		// TODO Auto-generated method stub
+		this.mcSocket.leaveGroup(inetAddress); // Abandonamos el grupo
+		this.mcSocket.close(); // Cerramos el socket
+		System.out.println("Saliendo del receptor Multicast");
 		
 	}
-	// Atributos
-	
-	// Constructor
-
 }
